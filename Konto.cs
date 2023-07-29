44b476e544ae6c9f1f340747b9a0f40202131cf1
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-07-29 19:17:54 +0200

    Add files via upload

---
+++ Konto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kontoverwaltung
+{
+    class Konto
+    {
+
+        private decimal kontostand;
+
+        public void GeldEinzahlen(decimal betrag)
+        {
+            kontostand = kontostand + betrag;
+        }
+
+        public void GeldAuszahlen(decimal betrag)
+        {
+            kontostand -= betrag;
+        }
+
+        public decimal KontostandAnzeigen()
+        {
+            if (kontostand < 0)
+                Console.ForegroundColor = ConsoleColor.Red;
+            else
+                Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Account balance: {kontostand:f2}");
+            Console.ForegroundColor = ConsoleColor.White;
+            return kontostand;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Kontoverwaltung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Konto k1 = new Konto();
+
+            do
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Account management:\n----------------");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("1) Deposit money");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("2) Cash out money");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("3) Check account balance");
+                Console.WriteLine("4) Quit");
+                Console.Write("Please select: ");
+                string eingabe = Console.ReadLine();
+
+                if (eingabe == "1")
+                {
+
+                    Console.WriteLine("Deposit money, what amount?");
+                    decimal betrag = Convert.ToDecimal(Console.ReadLine());
+                    k1.GeldEinzahlen(betrag);
+                }
+                else if (eingabe == "2")
+                {
+                    Console.WriteLine("Paying out money, what amount? ");
+                    decimal betrag = Convert.ToDecimal(Console.ReadLine());
+                    k1.GeldAuszahlen(betrag);
+                }
+                else if (eingabe == "3")
+                {
+                    k1.KontostandAnzeigen();
+                    Console.WriteLine("Please press button ...");
+                    Console.ReadKey();
+                }
+                else if (eingabe == "4")
+                {
+                    return;
+                }else
+                {
+                    Console.WriteLine("Incorrect input");
+                }
+            } while (true);
+            
+
+        }
+    }
+
+
+}
